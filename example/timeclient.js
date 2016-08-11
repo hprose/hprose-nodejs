6cd83ca86be7b64ad6d900f64e32bdb4eb62fc66
Ma Bingyao <mabingyao@gmail.com>
2016-08-11 16:23:36 +0800

    Update example

--- example/timeclient.js
+++ example/timeclient.js
@@ -1,7 +1,7 @@
 /*jshint node:true, eqeqeq:true */
 'use strict';
 
-var hprose = require('hprose');
+var hprose = require('../lib/hprose');
 var client = hprose.Client.create("http://127.0.0.1:8080");
 var count = 0;
 client.subscribe('time', function(date) {
