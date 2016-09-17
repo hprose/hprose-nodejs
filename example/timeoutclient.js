d75176d82c9284ccb2144e50aaa2b59e9e440632
Ma Bingyao <mabingyao@gmail.com>
2016-09-17 17:03:12 +0800

    Fixed timeout bug of HalfDuplex socket client.

---
+++ example/timeoutclient.js
@@ -0,0 +1,17 @@
+/*jshint node:true, eqeqeq:true */
+'use strict';
+
+var hprose = require('../lib/hprose.js');
+var client = hprose.Client.create('ws://127.0.0.1:4321/', ['sum']);
+// client.fullDuplex = false;
+client.keepAlive = false;
+client.timeout = 600;
+
+client.sum(1, 2);
+client.sum(1, 2).then(function(result) {
+    console.log("1 + 2 = " + result);
+}).catch(function() {
+    client.sum(2, 3, function(result) {
+        console.log("2 + 3 = " + result);
+    }, { timeout: 20000 });
+})
---
+++ example/timeoutserver.js
@@ -0,0 +1,19 @@
+/*jshint node:true, eqeqeq:true */
+'use strict';
+
+var hprose = require('../lib/hprose.js');
+
+function sum(a, b) {
+    var promise = new hprose.Future();
+    setTimeout(function() {
+        promise.resolve(a + b);
+    }, 1000);
+    return promise;
+}
+
+var server = hprose.Server.create("ws://0.0.0.0:4321");
+server.addFunction(sum);
+process.on('SIGINT', function() {
+  server.stop();
+});
+server.start();
