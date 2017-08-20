4ca8b87ea0df654439e1f9a3bb6a60890af0f3b3
Ma Bingyao <mabingyao@gmail.com>
2017-08-20 17:27:14 +0800

    Fixed #15

--- example/proxyclient.js
+++ example/proxyclient.js
@@ -2,11 +2,33 @@
 'use strict';
 
 var hprose = require('hprose');
-var client = hprose.Client.create('tcp://127.0.0.1:1234/', ['hello']);
+var client = hprose.Client.create('tcp://127.0.0.1:1234/', []);
 client.fullDuplex = true;
 client.maxPoolSize = 1;
-client.hello("World", function(result) {
+var proxy = client.useService();
+
+proxy.hello("World", function(result) {
     console.log(result);
 }, function(name, error) {
     console.error(error);
 });
+
+var weeks = {
+    'Monday': 'Mon',
+    'Tuesday': 'Tue',
+    'Wednesday': 'Wed',
+    'Thursday': 'Thu',
+    'Friday': 'Fri',
+    'Saturday': 'Sat',
+    'Sunday': 'Sun',
+};
+
+proxy.swapKeyAndValue.onsuccess = function(result, args) {
+    console.log(weeks.constructor, weeks);
+    console.log(result.constructor, result);
+    console.log(args.constructor, args);
+};
+
+proxy.swapKeyAndValue.byref = true;
+
+proxy.swapKeyAndValue(weeks);
