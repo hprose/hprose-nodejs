1bba92858b87a0bd78eba4aaf37d7d17fc7e45f3
asml <o7007@vip.qq.com>
2018-03-28 22:30:15 +0800

    fixed a bug of asyncfunction of httpserver

--- example/httpclient.js
+++ example/httpclient.js
@@ -4,16 +4,32 @@
 var hprose = require('../lib/hprose.js');
 var client = hprose.Client.create('http://127.0.0.1:8080/', []);
 client.simple = true;
-client.on('error', function(func, e) {
+client.on('error', function (func, e) {
     console.log(func, e);
 });
-hprose.co(function*() {
-  var proxy = client.useService(['hello']);
-  var start = new Date().getTime();
-  for (var i = 0; i < 100; i++) {
-      var result = yield proxy.hello(i);
-      console.log(result);
-  }
-  var end = new Date().getTime();
-  console.log("time: " + (end - start));
+hprose.co(function* () {
+    var proxy = client.useService(['Hello','Asynchello']);
+    //console.log(proxy);
+    
+    {
+        let start = new Date().getTime();
+        for (var i = 0; i < 32; i++) {
+            let result = yield proxy.Hello(i);
+            console.log(result);
+        }
+        let end = new Date().getTime();
+        console.log("time: " + (end - start));
+    }
+    // */
+    
+    {
+        let start = new Date().getTime();
+        for (var i = 0; i < 32; i++) {
+            let result = yield proxy.Asynchello(i);
+            console.log(result);
+        }
+        let end = new Date().getTime();
+        console.log("time: " + (end - start));
+    }
+    // */
 });
